6eb9a64704ed790a8296d3c26e756ea9771c5772
roman_ushakov <roman.ushakov.899@gmail.com>
2025-12-05 22:13:37 +0100

    read points from file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <raymath.h>
+#include <stdlib.h>
+#include <string.h>
 #include <vector>
+
+#define HEX_COLOR(c) ((Color) { \
+    (unsigned char) (c >> 24), \
+    (unsigned char) ((c >> 16) & 0xff)  , \
+    (unsigned char) ((c >> 8) & 0xff), \
+    (unsigned char) (c & 0xff) \
+})
 
 typedef struct {
     Vector2 pos;
@@ -36,11 +45,117 @@ std::vector<std::vector<size_t>> gen_pairs(std::vector<Point> points) {
     return pairs;
 }
 
-int main() {
-    // Vector2 v = {.x = 4, .y = 3};
+size_t take_until(char* text, char* buf, size_t off, char stop_char) {
+    size_t i = 0;
+    while(text[off + i] != stop_char && text[off + i] != '\0') {
+        buf[i] = text[off + i];
+        i++;
+    }
+    return i + 1;
+}
 
-    // printf("N, %f, %f", Vector2Normalize(v).x, Vector2Normalize(v).y);
-    // return 1;
+void print_point(Point point) {
+    printf("Point{\n");
+    printf("  .pos.x = %.2f\n", point.pos.x);
+    printf("  .pos.y = %.2f\n", point.pos.y);
+    printf("  .dir.x = %.2f\n", point.dir.x);
+    printf("  .dir.y = %.2f\n", point.dir.y);
+    printf("  .color = 0x%.2x%.2x%.2x%.2x\n", point.color.r, point.color.g, point.color.b, point.color.a);
+    printf("  .radius = %.2f\n", point.radius);
+    printf("}\n");
+}
+
+std::vector<Point> read_points(const char* file_path) {
+    FILE* points_fd = fopen(file_path, "r");
+    if (points_fd == NULL) {
+        perror("File error");
+        exit(1);
+    }
+
+    fseek(points_fd, 0, SEEK_END);
+
+    size_t file_size = ftell(points_fd);
+
+    fseek(points_fd, 0, SEEK_SET);
+
+    char* points_text = (char*)calloc(1, file_size + 1);
+    if (points_text == NULL) {
+        perror("Malloc failed");
+        exit(1);
+    }
+
+    size_t bytes_read = fread(points_text, 1, file_size, points_fd);
+    if (bytes_read != file_size) {
+        fprintf(stderr, "read %ld/%ld\n", bytes_read, file_size);
+        exit(1);
+    }
+
+    fclose(points_fd);
+
+    std::vector<Point> points;
+
+    size_t i = 0;
+    while(i < file_size) {
+        Point point = {};
+
+        char buf[256];
+
+        // ---- point.pos.x
+
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, ',');
+
+        point.pos.x = atof(buf);
+
+        // ---- point.pos.y
+
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, ',');
+        point.pos.y = atof(buf);
+
+        // ---- point.dir.x
+
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, ',');
+        point.dir.x = atof(buf);
+
+        // ---- point.dir.y
+
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, ',');
+        point.dir.y = atof(buf);
+
+        // ---- point.color
+
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, ',');
+        point.color = HEX_COLOR(strtol(buf, 0, 16));
+
+        // ---- point.radius
+        memset(buf, 0, 256);
+        
+        i += take_until(points_text, buf, i, '\n');
+        point.radius = atof(buf);
+
+        points.push_back(point);
+
+    }
+
+    free(points_text);
+
+    return points;
+}
+
+int main() {
+    std::vector<Point> points = read_points("./points.txt");
+
+    std::vector<std::vector<size_t>> pairs = gen_pairs(points);
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
     int windowWidth = 1200;
@@ -49,44 +164,6 @@ int main() {
     InitWindow(windowWidth, windowHeight, "Floating dots");
 
     SetTargetFPS(60);
-
-    std::vector<Point> points;
-    points.push_back({
-        .pos = {.x = 200, .y = 800},
-        .dir = {.x = 1, .y = -1},
-        .color = YELLOW,
-        .radius = 50.0,
-	});
-
-    points.push_back({
-        .pos = {.x = 800, .y = 200},
-        .dir = {.x = -1, .y = 1},
-        .color = YELLOW,
-        .radius = 50.0,
-	});
-
-    points.push_back({
-        .pos = {.x = 1000, .y = 800},
-        .dir = {.x = 1, .y = 1},
-        .color = YELLOW,
-        .radius = 50.0,
-	});
-
-    points.push_back({
-        .pos = {.x = 1100, .y = 200},
-        .dir = {.x = 1, .y = 1},
-        .color = YELLOW,
-        .radius = 30.0,
-	});
-
-    points.push_back({
-        .pos = {.x = 1100, .y = 400},
-        .dir = {.x = 1, .y = 1},
-        .color = YELLOW,
-        .radius = 20.0,
-	});
-
-    std::vector<std::vector<size_t>> pairs = gen_pairs(points);
 
     while (!WindowShouldClose()) {
         for (size_t i = 0; i < points.size(); i++) {
